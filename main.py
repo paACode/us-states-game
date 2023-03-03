af9434f352c8f4c89e9172b6c95ca6caf96f102c
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-03-03 11:19:33 +0100

    Master: Basic Game Functionality implemented

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas
 
 def get_us_state_information(selected_state):
     """Returns a Dataframe with Index, State, X-Coordinate and Y-Coordinate of selected State"""
-    return map_data[map_data.state.str.lower() == selected_state]
+    return map_data[map_data.state.str.title() == selected_state]
 
 
 def correct_us_state(information):
@@ -13,8 +13,20 @@ def correct_us_state(information):
     return True
 
 
-def get_coordinates(information):
-    return int(information.x), int(information.y)
+def add_state_label(information):
+    label = turtle.Turtle()
+    label.penup()
+    label.hideturtle()
+    label.goto(int(information.x), int(information.y))
+    label.write(answer_state)
+
+
+def write_success_message():
+    label = turtle.Turtle()
+    label.penup()
+    label.color("green")
+    label.hideturtle()
+    label.write("Great Work!!!", font=('Arial', 40, 'normal'), align="center")
 
 
 if __name__ == '__main__':
@@ -29,10 +41,13 @@ if __name__ == '__main__':
     score = 0
 
     while game_is_on:
-        answer_state = screen.textinput(title=f"Score {score}/50", prompt="Wat is another states name?").lower()
+        answer_state = screen.textinput(title=f"Score {score}/50", prompt="Wat is another states name?").title()
         state_information = get_us_state_information(selected_state=answer_state)
         if correct_us_state(information=state_information):
-            x_cor, y_cor = get_coordinates(information=state_information)
-            print(x_cor,y_cor)
+            add_state_label(information=state_information)
+            score += 1
+        if score >= 50:
+            write_success_message()
+            game_is_on = False
 
     turtle.mainloop()
