2002b2e1e3b9e48a035ab2d5185efd3a1c82be7b
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-03-11 18:29:48 +0100

    playing around more but still not working, not worth it to use list comprehension in this case, needed to restructure my code

--- main.py
+++ main.py
@@ -26,13 +26,7 @@ def write_success_message():
 
 
 def guessed_states_add_label():
-    [add_state_label(state) for state in guessed_states]
-
-def add_all_guessed_states_to_csv():
-    [pandas.concat([guessed_states_dataframe, get_selected_state_information(state)])
-     for state in guessed_states]
-    print(type(guessed_states_dataframe))
-    guessed_states_dataframe.to_csv("guessed_states.csv")
+    [add_state_label(state[1]) for state in guessed_states]
 
 
 if __name__ == '__main__':
@@ -41,14 +35,15 @@ if __name__ == '__main__':
     image = "blank_states_img.gif"
     screen.addshape(image)
     turtle.shape(image)
+    us_map_data = pandas.read_csv("50_states.csv")
+    all_us_states = us_map_data.state.values.tolist()
+    guessed_states = []
+    guessed_states_dataframe_list = pandas.DataFrame()
+
     if file_exists("guessed_states.csv"):
         guessed_states = pandas.read_csv("guessed_states.csv").values.tolist()
         guessed_states_add_label()
 
-    us_map_data = pandas.read_csv("50_states.csv")
-    all_us_states = us_map_data.state.values.tolist()
-    guessed_states = []
-    guessed_states_dataframe = pandas.DataFrame()
     score = len(guessed_states)
     game_is_on = True
 
@@ -57,7 +52,9 @@ if __name__ == '__main__':
 
         if answer_state == "exit".title():
             game_is_on = False
-
+            guessed_states_dataframe_list = [get_selected_state_information(state) for state in guessed_states]
+            guessed_states_dataframe = pandas.concat(guessed_states_dataframe_list)
+            guessed_states_dataframe.to_csv("guessed_states.csv")
         elif answer_state in all_us_states:
             guessed_states.append(answer_state)
             add_state_label(text=answer_state)
